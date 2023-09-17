6bae7a44789bb236ed3b03fc40ffe2cabd5ca178
Raghav Agarwal <raghavagrawal406@gmail.com>
2023-09-17 13:07:35 +0530

    changes

--- assets/js/dice.js
+++ assets/js/dice.js
@@ -1,5 +1,11 @@
-document.querySelector(".btn-outline-success").addEventListener("click", playGame);
+document.querySelector(".btn-outline-success").addEventListener("click", function(){
+    clickSound();
+    setTimeout(function(){
+        startGame();
+    }, 1000);
+});
 document.querySelector(".btn-outline-danger").addEventListener("click", reset);
+
 function getRandomNumber(){
     var rn = (Math.random() * 6) + 1;
     rn = Math.floor(rn);
@@ -14,8 +20,8 @@ function declareWinner(winner){
     }    
 }
 function changeImage(p1, p2){
-    var path1 = "./assets/images/dice"+p1+".png";
-    var path2 = "./assets/images/dice"+p2+".png";
+    var path1 = "assets/images/dice/dice"+p1+".png";
+    var path2 = "assets/images/dice/dice"+p2+".png";
     console.log(path1);
     console.log(path2);
     document.querySelector(".img1").setAttribute("src", path1);
@@ -31,14 +37,29 @@ function changeImage(p1, p2){
         declareWinner(0);
     }
 }
+function clickSound(){
+    var audio = new Audio("assets/sounds/mouse-click.mp3");
+    audio.play();
+}
+function rollSound(){
+    var audio = new Audio("assets/sounds/dice/dice_roll.mp3");
+    audio.play();
+}
 function playGame(){
     var player1 = getRandomNumber(); 
     var player2 = getRandomNumber(); 
     changeImage(player1, player2);
 }
+function startGame(){
+    rollSound();
+    setTimeout(function(){
+        playGame();
+    }, 1500);
+}
 function reset(){
+    clickSound();
     document.querySelector("h1").innerHTML = "Roll the Dice";
-    var path = "./assets/images/dice"+6+".png";
+    var path = "assets/images/dice/dice"+6+".png";
     document.querySelector(".img1").setAttribute("src", path);
     document.querySelector(".img2").setAttribute("src", path);
 }
---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+// function handleClick(){
+//     var audio = new Audio("assets/sounds/mouse-click.mp3");
+//     audio.play();
+// }
+// var allButtons = document.querySelectorAll(".btn");
+// for(var i = 0; i<allButtons.length; i++){
+//     allButtons[i].addEventListener("click", handleClick);
+// }
